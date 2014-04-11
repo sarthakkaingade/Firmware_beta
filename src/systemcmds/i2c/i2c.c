39908e96702b85786e9e19169f078d39250315e7
Sarthak Kaingade <sarthakkaingade@yahoo.com>
2014-04-11 17:39:46 +0530

    Updated i2c system command

--- src/systemcmds/i2c/i2c.c
+++ src/systemcmds/i2c/i2c.c
@@ -1,5 +1,5 @@
 /****************************************************************************
- *
+ *   Copyright (C) 2013 Navstik Development Team. Based on PX4 port.
  *   Copyright (c) 2012, 2013 PX4 Development Team. All rights reserved.
  *   Author: Lorenz Meier <lm@inf.ethz.ch>
  *
@@ -57,11 +57,11 @@
 #include "systemlib/systemlib.h"
 #include "systemlib/err.h"
 
-#ifndef PX4_I2C_BUS_ONBOARD
-#  error PX4_I2C_BUS_ONBOARD not defined, no device interface
+#ifndef NAVSTIK_I2C_BUS_MPU
+#  error NAVSTIK_I2C_BUS_MPU not defined, no device interface
 #endif
-#ifndef PX4_I2C_OBDEV_PX4IO
-#  error PX4_I2C_OBDEV_PX4IO not defined
+#ifndef NAVSTIK_I2C_BUS_SENSORS
+#  error NAVSTIK_I2C_BUS_SENSORS not defined
 #endif
 
 __EXPORT int i2c_main(int argc, char *argv[]);
@@ -73,7 +73,7 @@ static struct i2c_dev_s *i2c;
 int i2c_main(int argc, char *argv[])
 {
 	/* find the right I2C */
-	i2c = up_i2cinitialize(PX4_I2C_BUS_ONBOARD);
+	i2c = up_i2cinitialize(NAVSTIK_I2C_BUS_MPU);
 
 	if (i2c == NULL)
 		errx(1, "failed to locate I2C bus");
@@ -81,13 +81,13 @@ int i2c_main(int argc, char *argv[])
 	usleep(100000);
 
 	uint8_t buf[] = { 0, 4};
-	int ret = transfer(PX4_I2C_OBDEV_PX4IO, buf, sizeof(buf), NULL, 0);
+	int ret = transfer(NAVSTIK_I2C_BUS_SENSORS, buf, sizeof(buf), NULL, 0);
 
 	if (ret)
 		errx(1, "send failed - %d", ret);
 
 	uint32_t val;
-	ret = transfer(PX4_I2C_OBDEV_PX4IO, NULL, 0, (uint8_t *)&val, sizeof(val));
+	ret = transfer(NAVSTIK_I2C_BUS_SENSORS, NULL, 0, (uint8_t *)&val, sizeof(val));
 	if (ret)
 		errx(1, "recive failed - %d", ret);
 
